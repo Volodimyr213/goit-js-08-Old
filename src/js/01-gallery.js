79d9c64813da1323faf9d17e39bce0947392053b
Volodimyr213 <Terminal213@gmail.com>
2023-08-29 11:39:42 +0300

    homW1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,43 @@
+
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+
 
 console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+const ulElemHtml = document.querySelector(".gallery")
+
+
+
+  function createMarcup(arr){
+   
+return arr.map( ({original,preview,description}) =>
+
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+       alt="${description}"
+      />
+    </a>
+  </li>` 
+    ).join(``)
+   
+  };
+
+  ulElemHtml.insertAdjacentHTML(`beforeend`, createMarcup(galleryItems))
+
+
+  
+console.log (SimpleLightbox)
+const lightbox = new SimpleLightbox('.gallery a', {
+    caption: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
